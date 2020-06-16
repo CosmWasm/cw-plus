74e0e09af1336f166cdc4fe4a6f41585168a6fd6
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-06-16 16:22:21 +0200

    Update schemas

--- contracts/cw20-base/examples/schema.rs
+++ contracts/cw20-base/examples/schema.rs
@@ -3,8 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use cw20_base::msg::{CountResponse, HandleMsg, InitMsg, QueryMsg};
-use cw20_base::state::State;
+use cw20::{BalanceResponse, MetaResponse};
+use cw20_base::msg::{HandleMsg, InitMsg, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -15,6 +15,6 @@ fn main() {
     export_schema(&schema_for!(InitMsg), &out_dir);
     export_schema(&schema_for!(HandleMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
-    export_schema(&schema_for!(State), &out_dir);
-    export_schema(&schema_for!(CountResponse), &out_dir);
+    export_schema(&schema_for!(BalanceResponse), &out_dir);
+    export_schema(&schema_for!(MetaResponse), &out_dir);
 }
