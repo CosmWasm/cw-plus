3eb2a427ae62fe136d22452e92ecad046f67c5cd
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-06-16 10:56:46 +0200

    Update schemas

--- packages/cw20/examples/schema.rs
+++ packages/cw20/examples/schema.rs
@@ -4,6 +4,7 @@ use std::fs::create_dir_all;
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
 use cw20::msg::HandleMsg;
+use cw20::query::{AllowanceResponse, BalanceResponse, MetaResponse, MinterResponse, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -12,4 +13,9 @@ fn main() {
     remove_schemas(&out_dir).unwrap();
 
     export_schema(&schema_for!(HandleMsg), &out_dir);
+    export_schema(&schema_for!(QueryMsg), &out_dir);
+    export_schema(&schema_for!(AllowanceResponse), &out_dir);
+    export_schema(&schema_for!(BalanceResponse), &out_dir);
+    export_schema(&schema_for!(MetaResponse), &out_dir);
+    export_schema(&schema_for!(MinterResponse), &out_dir);
 }
