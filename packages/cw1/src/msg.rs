3154c804694e8e01ed1467c24aeff8b825370673
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-07-07 20:12:27 +0200

    Add cw1 package for proxy contracts

---
+++ packages/cw1/src/msg.rs
@@ -0,0 +1,13 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cosmwasm_std::CosmosMsg;
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum Cw1HandleMsg {
+    /// Execute requests the contract to re-dispatch all these messages with the
+    /// contract's address as sender. Every implementation has it's own logic to
+    /// determine in
+    Execute { msgs: Vec<CosmosMsg> },
+}
