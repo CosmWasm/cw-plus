07d788e32e5e102ebcbc77523a73650e4d83aa54
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-07-24 13:32:02 +0200

    Fix up missing import

--- contracts/cw1-whitelist/examples/schema.rs
+++ contracts/cw1-whitelist/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use cw1_multisig::msg::{ConfigResponse, HandleMsg, InitMsg, QueryMsg};
+use cw1_whitelist::msg::{ConfigResponse, HandleMsg, InitMsg, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
