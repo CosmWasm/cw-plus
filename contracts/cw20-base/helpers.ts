b67466304e50f03ebb00ccd43af51d66514bf7d3
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-08-14 00:07:44 +0200

    Cleanup cw20-base helpers.ts

--- contracts/cw20-base/helpers.ts
+++ contracts/cw20-base/helpers.ts
@@ -48,16 +48,19 @@ interface Network {
 const useOptions = (options: Options): Network => {
 
   const loadOrCreateWallet = async (options: Options, filename: string, password: string): Promise<Secp256k1Wallet> => {
+    let encrypted: string;
     try {
-      const encrypted = fs.readFileSync(filename, 'utf8');
-      const wallet = await Secp256k1Wallet.deserialize(encrypted, password);
-      return wallet;
+      encrypted = fs.readFileSync(filename, 'utf8');
     } catch (err) {
+      // generate if no file exists
       const wallet = await Secp256k1Wallet.generate(12, options.hdPath, options.bech32prefix);
       const encrypted = await wallet.serialize(password);
       fs.writeFileSync(filename, encrypted, 'utf8');
       return wallet;
     }
+    // otherwise, decrypt the file (we cannot put deserialize inside try or it will over-write on a bad password)
+    const wallet = await Secp256k1Wallet.deserialize(encrypted, password);
+    return wallet;
   };
   
   const buildFeeTable = (options: Options): FeeTable => {
@@ -154,7 +157,7 @@ interface CW20Instance {
   // queries
   balance: (address?: string) => Promise<string>
   allowance: (owner: string, spender: string) => Promise<string>
-  token_info: () => Promise<any>
+  tokenInfo: () => Promise<any>
   minter: () => Promise<any>
 
   // actions
@@ -193,7 +196,7 @@ const CW20 = (client: SigningCosmWasmClient): CW20Contract => {
       return result.allowance;
     };
 
-    const token_info = async (): Promise<any> => {
+    const tokenInfo = async (): Promise<any> => {
       return client.queryContractSmart(contractAddress, {token_info: { }});
     };
 
@@ -238,7 +241,7 @@ const CW20 = (client: SigningCosmWasmClient): CW20Contract => {
       contractAddress,
       balance,
       allowance,
-      token_info,
+      tokenInfo,
       minter,
       mint,
       transfer,
@@ -274,91 +277,4 @@ const CW20 = (client: SigningCosmWasmClient): CW20Contract => {
   }
 
   return { upload, instantiate, use };
-}
-
-
-/*** this is demo code  ***/
-const demo = async () => {
-  console.log("Running demo....");
-  const client = await useOptions(coralnetOptions).setup("12345678");
-  console.log(client.senderAddress);
-  const account = await client.getAccount();
-  console.log(account);
-}
-
-const exampleStars = async () => {
-  console.log("Setup....");
-  const client = await useOptions(coralnetOptions).setup("12345678");
-
-  const cw20 = CW20(client);
-  const codeId = await cw20.upload();
-  console.log(`CodeId: ${codeId}`);
-
-  const initMsg: InitMsg = {
-    name: "Golden Stars",
-    symbol: "STAR",
-    decimals: 2,
-    // list of all validator self-delegate addresses - 100 STARs each!
-    initial_balances: [
-      { address: "coral1exta8hzrghyt5umd4jh55kfkmp0tv3hyg8krc5", amount: "10000"},
-      { address: "coral13mcejut8e5tncs59zcs4yn4envcd98vx682frk", amount: "10000"},
-      { address: "coral10zn0d2eeust0495crtr3zqz7t688hg0s53afrh", amount: "10000"},
-      { address: "coral1qvrcashqpemlkhrqphzv9n5nutdxpafmdefgcl", amount: "10000"},
-      { address: "coral14f8nvyy4c9pyn78dgv0k6syek3jjjrkyz747kj", amount: "10000"},
-      { address: "coral1e86v774dch5uwkks0cepw8mdz8a9flhhapvf6w", amount: "10000"},
-      { address: "coral1hf50trj7plz2sd8cmcvn7c8ruh3tjhc2nhyl7l", amount: "10000"},
-    ],
-    mint: {
-      minter: client.senderAddress,
-    },
-  };
-  const contract = await cw20.instantiate(codeId, initMsg, "STAR");
-  console.log(`Contract: ${contract.contractAddress}`);
-
-  console.log(await contract.balance("coral13mcejut8e5tncs59zcs4yn4envcd98vx682frk"));
-  console.log(await contract.balance());
-
-  // Setup....
-  // CodeId: 4
-  // Contract: coral16t7y0vrtpqjw2d7jvc2209yan9002339mg4mrv
-  // 10000
-  // 0
-}
-
-const usage = async () => {
-  const addr = "coral16t7y0vrtpqjw2d7jvc2209yan9002339mg4mrv";
-  const client = await useOptions(coralnetOptions).setup("12345678");
-  const stars = CW20(client).use(addr);
-  
-  console.log(`info: ${JSON.stringify(await stars.token_info())}`);
-  console.log(`minter: ${JSON.stringify(await stars.minter())}`);
-  const acct = "coral14f8nvyy4c9pyn78dgv0k6syek3jjjrkyz747kj";
-  console.log(`balance of ${acct}: ${await stars.balance(acct)}`);
-
-  console.log(`my balance: ${await stars.balance()}`);
-  console.log("minting myself 100 STAR");
-  const mintTx = await stars.mint(client.senderAddress, "10000");
-  console.log(`Tx: ${mintTx}`);
-  console.log(`my balance: ${await stars.balance()}`);
-
-  const lucky = "coral1hf50trj7plz2sd8cmcvn7c8ruh3tjhc2nhyl7l";
-  console.log(`balance of ${lucky}: ${await stars.balance(lucky)}`);
-  console.log("send 5 STAR to ${lucky}");
-  const transferTx = await stars.transfer(lucky, "500");
-  console.log(`Tx: ${transferTx}`);
-  console.log(`balance of ${lucky}: ${await stars.balance(lucky)}`);
-  console.log(`my balance: ${await stars.balance()}`);
-
-  // info: {"name":"Golden Stars","symbol":"STAR","decimals":2,"total_supply":"70000"}
-  // minter: {"minter":"coral15m4z2650nkcr7r6g5dyzf4qwcrcmrrjh6t7x0f","cap":null}
-  // balance of coral14f8nvyy4c9pyn78dgv0k6syek3jjjrkyz747kj: 10000
-  // my balance: 0
-  // minting myself 100 STAR
-  // Tx: 257283B98DB5D10412839ACC9667E0E5FFF001CC1BE4AFA4527157082C15F2FA
-  // my balance: 10000
-  // balance of coral1hf50trj7plz2sd8cmcvn7c8ruh3tjhc2nhyl7l: 10000
-  // send 5 STAR to ${lucky}
-  // Tx: 83FBD409BFEBF62AB6926C592788EF7DC378CBBFA1337A33931F45F84D79B17B
-  // balance of coral1hf50trj7plz2sd8cmcvn7c8ruh3tjhc2nhyl7l: 10500
-  // my balance: 9500
 }
