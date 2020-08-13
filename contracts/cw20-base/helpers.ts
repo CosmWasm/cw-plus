b7e4ab443bfc4a28df28fe4edc997a58f4f5c923
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-08-13 20:37:44 +0200

    Network setup script polished

--- contracts/cw20-base/helpers.ts
+++ contracts/cw20-base/helpers.ts
@@ -7,14 +7,18 @@
  * If you want to use this code inside an app, you will need several imports from https://github.com/CosmWasm/cosmjs
  */
 
+const path = require("path");
+
 interface Options {
   readonly httpUrl: string
-  readonly faucetUrl?: string
   readonly networkId: string
   readonly feeToken: string
   readonly gasPrice: number
   readonly bech32prefix: string
   readonly hdPath: readonly Slip10RawIndex[]
+  readonly faucetToken: string
+  readonly faucetUrl?: string
+  readonly defaultKeyFile: string
 }
   
 const coralnetOptions: Options = {
@@ -23,58 +27,104 @@ const coralnetOptions: Options = {
   feeToken: 'ushell',
   gasPrice: 0.025,
   bech32prefix: 'coral',
+  faucetToken: 'SHELL',
   faucetUrl: 'https://faucet.coralnet.cosmwasm.com/credit',
   hdPath: makeCosmoshubPath(0),
+  defaultKeyFile: path.join(process.env.HOME, ".coral.key"),
 }
 
-const connect = async (
-  wallet: Secp256k1Wallet,
-  options: Options
-): Promise<SigningCosmWasmClient> => {
-  const feeTable = buildFeeTable(options);
-  const [{ address }] = await wallet.getAccounts();
-
-  const client = new SigningCosmWasmClient(
-    options.httpUrl,
-    address,
-    wallet,
-    feeTable
-  );
-  return client;
+interface Network {
+  setup: (password: string, filename?: string) => Promise<{
+    client: SigningCosmWasmClient,
+    wallet: Secp256k1Wallet,
+  }>
 }
 
-const buildFeeTable = (options: Options): FeeTable => {
-  const { feeToken, gasPrice } = options;
-  const stdFee = (gas: number, denom: string, price: number) => {
-    const amount = Math.floor(gas * price)
-    return {
-      amount: [{ amount: amount.toString(), denom: denom }],
-      gas: gas.toString(),
+const useOptions = (options: Options): Network => {
+
+  const loadOrCreateWallet = async (options: Options, filename: string, password: string): Promise<Secp256k1Wallet> => {
+    try {
+      const encrypted = fs.readFileSync(filename, 'utf8');
+      const wallet = await Secp256k1Wallet.deserialize(encrypted, password);
+      return wallet;
+    } catch (err) {
+      const wallet = await Secp256k1Wallet.generate(12, options.hdPath, options.bech32prefix);
+      const encrypted = await wallet.serialize(password);
+      fs.writeFileSync(filename, encrypted, 'utf8');
+      return wallet;
     }
+  };
+  
+  const buildFeeTable = (options: Options): FeeTable => {
+    const { feeToken, gasPrice } = options;
+    const stdFee = (gas: number, denom: string, price: number) => {
+      const amount = Math.floor(gas * price)
+      return {
+        amount: [{ amount: amount.toString(), denom: denom }],
+        gas: gas.toString(),
+      }
+    }
+  
+    return {
+      upload: stdFee(1000000, feeToken, gasPrice),
+      init: stdFee(500000, feeToken, gasPrice),
+      migrate: stdFee(500000, feeToken, gasPrice),
+      exec: stdFee(200000, feeToken, gasPrice),
+      send: stdFee(80000, feeToken, gasPrice),
+      changeAdmin: stdFee(80000, feeToken, gasPrice),
+    }
+  };
+
+  const connect = async (
+    wallet: Secp256k1Wallet,
+    options: Options
+  ): Promise<SigningCosmWasmClient> => {
+    const feeTable = buildFeeTable(options);
+    const [{ address }] = await wallet.getAccounts();
+  
+    const client = new SigningCosmWasmClient(
+      options.httpUrl,
+      address,
+      wallet,
+      feeTable
+    );
+    return client;
+  };
+  
+  const hitFaucet = async (
+    faucetUrl: string,
+    address: string,
+    ticker: string
+  ): Promise<void> => {
+    await axios.post(faucetUrl, { ticker, address });
+  }
+  
+  const setup = async (password: string, filename?: string): Promise<{
+    client: SigningCosmWasmClient,
+    wallet: Secp256k1Wallet,
+  }> => {
+    const keyfile = filename || options.defaultKeyFile;
+    const wallet = await loadOrCreateWallet(coralnetOptions, keyfile, password);
+    const client = await connect(wallet, coralnetOptions);
+
+    // ensure we have some tokens
+    if (options.faucetUrl) {
+      const account = await client.getAccount();
+      if (!account) {
+        console.log(`Getting ${options.feeToken} from faucet`);
+        await hitFaucet(options.faucetUrl, client.senderAddress, options.faucetToken);
+      }  
+    }
+
+    return {
+      client,
+      wallet,
+    };    
   }
 
-  return {
-    upload: stdFee(1000000, feeToken, gasPrice),
-    init: stdFee(500000, feeToken, gasPrice),
-    migrate: stdFee(500000, feeToken, gasPrice),
-    exec: stdFee(200000, feeToken, gasPrice),
-    send: stdFee(80000, feeToken, gasPrice),
-    changeAdmin: stdFee(80000, feeToken, gasPrice),
-  }
+  return {setup};
 }
 
-const loadOrCreateWallet = async (options: Options, filename: string, password: string): Promise<Secp256k1Wallet> => {
-  try {
-    const encrypted = fs.readFileSync(filename, 'utf8');
-    const wallet = await Secp256k1Wallet.deserialize(encrypted, password);
-    return wallet;
-  } catch (err) {
-    const wallet = await Secp256k1Wallet.generate(12, options.hdPath, options.bech32prefix);
-    const encrypted = await wallet.serialize(password);
-    fs.writeFileSync(filename, encrypted, 'utf8');
-    return wallet;
-  }
-}
 
 const downloadWasm = async (url: string): Promise<Uint8Array> => {
   const r = await axios.get(url, { responseType: 'arraybuffer' })
@@ -84,23 +134,15 @@ const downloadWasm = async (url: string): Promise<Uint8Array> => {
   return r.data
 }
 
-const hitFaucet = async (
-  faucetUrl: string,
-  address: string,
-  ticker: string
-): Promise<void> => {
-  const r = await axios.post(faucetUrl, { ticker, address });
-  console.log(r.status);
-  console.log(r.data);
-}
 
 
 /*** this is demo code  ***/
 const main = async () => {
   console.log("Running demo....");
-  const wallet = await loadOrCreateWallet(coralnetOptions, "demo.key", "12345678");
-  const client = await connect(wallet, coralnetOptions);
+  const coralnet = useOptions(coralnetOptions);
+  const { client, wallet } = await coralnet.setup("12345678");
   console.log(client.senderAddress);
+  console.log(wallet.mnemonic);
   const account = await client.getAccount();
   console.log(account);
 }
