e2d5347cc90ecc8f7a8c2cf1b40e18fed538f88f
Mauro Lacy <mauro@lacy.com.es>
2020-08-19 07:09:00 +0100

    Add cargo config / schema generation

---
+++ contracts/atomic-swap/examples/schema.rs
@@ -0,0 +1,21 @@
+use std::env::current_dir;
+use std::fs::create_dir_all;
+
+use cosmwasm_schema::{export_schema, export_schema_with_title, remove_schemas, schema_for};
+
+use atomic_swap::msg::HandleMsg;
+use atomic_swap::msg::InitMsg;
+
+fn main() {
+    let mut out_dir = current_dir().unwrap();
+    out_dir.push("schema");
+    create_dir_all(&out_dir).unwrap();
+    remove_schemas(&out_dir).unwrap();
+
+    export_schema(&schema_for!(InitMsg), &out_dir);
+    export_schema_with_title(&mut schema_for!(HandleMsg), &out_dir, "HandleMsg");
+    /*
+    export_schema(&schema_for!(ReleaseMsg), &out_dir);
+    export_schema(&schema_for!(RefundMsg), &out_dir);
+    */
+}
