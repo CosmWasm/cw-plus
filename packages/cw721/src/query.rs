41cde2c29d923a382bcb41f220f0bfffa664c5d4
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-08-21 21:51:40 +0200

    Clean up query responses

--- packages/cw721/src/query.rs
+++ packages/cw721/src/query.rs
@@ -85,27 +85,15 @@ pub struct NftInfoResponse {
     /// "A URI pointing to a resource with mime type image/* representing the asset to which this
     /// NFT represents. Consider making any images at a width between 320 and 1080 pixels and aspect
     /// ratio between 1.91:1 and 4:5 inclusive.
-    /// TODO: Custom URI type??
-    pub image: String,
-    // TODO: make this extensible with extra fields? (Response must be a superset of this)
+    /// TODO: Use https://docs.rs/url_serde for type-safety
+    pub image: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
 /// TODO: cleaner way of combining the two structs than cut and paste?
 pub struct AllNftInfoResponse {
-    /// Owner of the token
-    pub owner: HumanAddr,
-    /// If set this address is approved to transfer/send the token as well
-    pub approval: Option<Approval>,
-
-    /// Identifies the asset to which this NFT represents
-    pub name: String,
-    /// Describes the asset to which this NFT represents
-    pub description: String,
-    /// "A URI pointing to a resource with mime type image/* representing the asset to which this
-    /// NFT represents. Consider making any images at a width between 320 and 1080 pixels and aspect
-    /// ratio between 1.91:1 and 4:5 inclusive.
-    /// TODO: Custom URI type??
-    pub image: String,
-    // TODO: make this extensible with extra fields? (Response must be a superset of this)
+    /// Who can transfer the token
+    pub access: OwnerOfResponse,
+    /// Data on the token itself,
+    pub info: NftInfoResponse,
 }
