75fee37db0e4776a2fc994a6c0094a612e585e0f
Mauro Lacy <mauro@lacy.com.es>
2020-08-23 20:28:12 +0200

    release() integration test

--- contracts/atomic-swap/tests/integration.rs
+++ contracts/atomic-swap/tests/integration.rs
@@ -17,12 +17,13 @@
 //!      });
 //! 4. Anywhere you see query(&deps, ...) you must replace it with query(&mut deps, ...)
 
-use atomic_swap::msg::{CreateMsg, HandleMsg, InitMsg};
-use cosmwasm_std::{coins, StdError};
+use cosmwasm_std::{coins, BankMsg, Coin, CosmosMsg, Env, StdError};
 use cosmwasm_std::{log, HandleResponse, HandleResult, HumanAddr, InitResponse};
 use cosmwasm_vm::testing::{handle, init, mock_env, mock_instance};
-
 use sha2::{Digest, Sha256};
+
+use atomic_swap::msg::{CreateMsg, HandleMsg, InitMsg};
+use cosmwasm_std::testing::MOCK_CONTRACT_ADDR;
 
 // This line will test the output of cargo wasm
 static WASM: &[u8] =
@@ -36,6 +37,12 @@ fn preimage() -> String {
 
 fn real_hash() -> String {
     hex::encode(&Sha256::digest(&hex::decode(preimage()).unwrap()))
+}
+
+fn mock_env_height<U: Into<HumanAddr>>(sender: U, sent: &[Coin], height: u64) -> Env {
+    let mut env = mock_env(sender, sent);
+    env.block.height = height;
+    env
 }
 
 #[test]
@@ -162,5 +169,104 @@ fn test_create() {
             assert_eq!(msg, "Atomic swap already exists".to_string())
         }
         Err(e) => panic!("unexpected error: {:?}", e),
+    }
+}
+
+#[test]
+fn test_release() {
+    let mut deps = mock_instance(WASM, &[]);
+
+    let env = mock_env("anyone", &[]);
+    let _: InitResponse = init(&mut deps, env, InitMsg {}).unwrap();
+
+    let sender = HumanAddr::from("sender0001");
+    let balance = coins(1000, "tokens");
+
+    let env = mock_env(&sender, &balance);
+    let create = CreateMsg {
+        id: "swap0001".to_string(),
+        hash: real_hash(),
+        recipient: "rcpt0001".into(),
+        end_time: 0,
+        end_height: 123456,
+    };
+    let _: HandleResponse =
+        handle(&mut deps, env.clone(), HandleMsg::Create(create.clone())).unwrap();
+
+    // Cannot release, wrong id
+    let release = HandleMsg::Release {
+        id: "swap0002".to_string(),
+        preimage: preimage(),
+    };
+    let res: HandleResult = handle(&mut deps, env.clone(), release);
+    match res {
+        Ok(_) => panic!("expected error"),
+        Err(StdError::NotFound { .. }) => {}
+        Err(e) => panic!("unexpected error: {:?}", e),
+    }
+
+    // Cannot release, invalid hash
+    let release = HandleMsg::Release {
+        id: "swap0001".to_string(),
+        preimage: "bu115h17".to_string(),
+    };
+    let res: HandleResult = handle(&mut deps, env.clone(), release);
+    match res {
+        Ok(_) => panic!("expected error"),
+        Err(StdError::GenericErr { msg, .. }) => assert_eq!(
+            msg,
+            "Error parsing hash: Invalid character \'u\' at position 1".to_string()
+        ),
+        Err(e) => panic!("unexpected error: {:?}", e),
+    }
+
+    // Cannot release, wrong hash
+    let release = HandleMsg::Release {
+        id: "swap0001".to_string(),
+        preimage: hex::encode(b"This is 32 bytes, but incorrect."),
+    };
+    let res: HandleResult = handle(&mut deps, env.clone(), release);
+    match res {
+        Ok(_) => panic!("expected error"),
+        Err(StdError::GenericErr { msg, .. }) => assert_eq!(msg, "Invalid preimage".to_string()),
+        Err(e) => panic!("unexpected error: {:?}", e),
+    }
+
+    // Cannot release, expired
+    let env = mock_env_height(&sender, &balance, 123457);
+    let release = HandleMsg::Release {
+        id: "swap0001".to_string(),
+        preimage: preimage(),
+    };
+    let res: HandleResult = handle(&mut deps, env.clone(), release);
+    match res {
+        Ok(_) => panic!("expected error"),
+        Err(StdError::GenericErr { msg, .. }) => assert_eq!(msg, "Atomic swap expired".to_string()),
+        Err(e) => panic!("unexpected error: {:?}", e),
+    }
+
+    // Can release, valid id, valid hash, and not expired
+    let env = mock_env("somebody", &balance);
+    let release = HandleMsg::Release {
+        id: "swap0001".to_string(),
+        preimage: preimage(),
+    };
+    let res: HandleResponse = handle(&mut deps, env.clone(), release.clone()).unwrap();
+    assert_eq!(log("action", "release"), res.log[0]);
+    assert_eq!(1, res.messages.len());
+    assert_eq!(
+        res.messages[0],
+        CosmosMsg::Bank(BankMsg::Send {
+            from_address: HumanAddr::from(MOCK_CONTRACT_ADDR),
+            to_address: create.recipient,
+            amount: balance,
+        })
+    );
+
+    // Cannot release again
+    let res: HandleResult = handle(&mut deps, env.clone(), release);
+    match res.unwrap_err() {
+        StdError::NotFound { .. } => {}
+        e => panic!("Expected NotFound, got {}", e),
     }
 }
