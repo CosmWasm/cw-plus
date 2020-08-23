1d8dbf1a14fca4a9cffb8286547e8c630ab1201a
Mauro Lacy <mauro@lacy.com.es>
2020-08-23 11:35:49 +0200

    Add state unit tests

--- contracts/atomic-swap/src/state.rs
+++ contracts/atomic-swap/src/state.rs
@@ -32,7 +32,7 @@ pub fn atomic_swaps<S: Storage>(storage: &mut S) -> Bucket<S, AtomicSwap> {
     bucket(PREFIX_SWAP, storage)
 }
 
-/// returns a bucket with all swaps (query by id)
+/// Returns a bucket with all swaps (query by id)
 /// (read-only version for queries)
 pub fn atomic_swaps_read<S: ReadonlyStorage>(storage: &S) -> ReadonlyBucket<S, AtomicSwap> {
     bucket_read(PREFIX_SWAP, storage)
@@ -44,4 +44,49 @@ pub fn all_swap_ids<S: ReadonlyStorage>(storage: &S) -> StdResult<Vec<String>> {
         .range(None, None, Order::Ascending)
         .map(|(k, _)| String::from_utf8(k).map_err(|_| StdError::invalid_utf8("Parsing swap id")))
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use cosmwasm_std::testing::MockStorage;
+    use cosmwasm_std::Binary;
+
+    #[test]
+    fn test_no_swap_ids() {
+        let storage = MockStorage::new();
+        let ids = all_swap_ids(&storage).unwrap();
+        assert_eq!(0, ids.len());
+    }
+
+    fn dummy_swap() -> AtomicSwap {
+        AtomicSwap {
+            recipient: CanonicalAddr(Binary(b"recip".to_vec())),
+            source: CanonicalAddr(Binary(b"source".to_vec())),
+            hash: "hash".to_string(),
+            ..AtomicSwap::default()
+        }
+    }
+
+    #[test]
+    fn test_all_swap_ids() {
+        let mut storage = MockStorage::new();
+        atomic_swaps(&mut storage)
+            .save("lazy".as_bytes(), &dummy_swap())
+            .unwrap();
+        atomic_swaps(&mut storage)
+            .save("assign".as_bytes(), &dummy_swap())
+            .unwrap();
+        atomic_swaps(&mut storage)
+            .save("zen".as_bytes(), &dummy_swap())
+            .unwrap();
+
+        let ids = all_swap_ids(&storage).unwrap();
+        assert_eq!(3, ids.len());
+        assert_eq!(
+            vec!["assign".to_string(), "lazy".to_string(), "zen".to_string()],
+            ids
+        )
+    }
 }
