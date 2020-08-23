7cd6f93614078489ac33f2f1a6a9d319f481a96a
Mauro Lacy <mauro@lacy.com.es>
2020-08-23 19:46:13 +0200

    create() integration test

--- contracts/atomic-swap/tests/integration.rs
+++ contracts/atomic-swap/tests/integration.rs
@@ -17,16 +17,26 @@
 //!      });
 //! 4. Anywhere you see query(&deps, ...) you must replace it with query(&mut deps, ...)
 
-use cosmwasm_std::InitResponse;
-use cosmwasm_vm::testing::{init, mock_env, mock_instance};
-//use cosmwasm_vm::{from_slice, Api, Storage};
-use atomic_swap::msg::InitMsg;
+use atomic_swap::msg::{CreateMsg, HandleMsg, InitMsg};
+use cosmwasm_std::{coins, StdError};
+use cosmwasm_std::{log, HandleResponse, HandleResult, HumanAddr, InitResponse};
+use cosmwasm_vm::testing::{handle, init, mock_env, mock_instance};
+
+use sha2::{Digest, Sha256};
 
 // This line will test the output of cargo wasm
 static WASM: &[u8] =
     include_bytes!("../../../target/wasm32-unknown-unknown/release/atomic_swap.wasm");
 // Uncomment this line instead to test productivized build from rust-optimizer
 // static WASM: &[u8] = include_bytes!("../contract.wasm");
+
+fn preimage() -> String {
+    hex::encode(b"This is a string, 32 bytes long.")
+}
+
+fn real_hash() -> String {
+    hex::encode(&Sha256::digest(&hex::decode(preimage()).unwrap()))
+}
 
 #[test]
 fn test_init() {
@@ -37,4 +47,120 @@ fn test_init() {
     let env = mock_env("anyone", &[]);
     let res: InitResponse = init(&mut deps, env, init_msg).unwrap();
     assert_eq!(0, res.messages.len());
+}
+
+#[test]
+fn test_create() {
+    let mut deps = mock_instance(WASM, &[]);
+
+    let env = mock_env("anyone", &[]);
+    let _: InitResponse = init(&mut deps, env, InitMsg {}).unwrap();
+
+    let sender = HumanAddr::from("sender0001");
+    let balance = coins(100, "tokens");
+
+    // Cannot create, invalid ids
+    let env = mock_env(&sender, &balance);
+    for id in vec!["sh", "atomic_swap_id_too_long"] {
+        let create = CreateMsg {
+            id: id.to_string(),
+            hash: real_hash(),
+            recipient: HumanAddr::from("rcpt0001"),
+            end_time: 0,
+            end_height: 123456,
+        };
+        let res: HandleResult = handle(&mut deps, env.clone(), HandleMsg::Create(create.clone()));
+        match res {
+            Ok(_) => panic!("expected error"),
+            Err(StdError::GenericErr { msg, .. }) => {
+                assert_eq!(msg, "Invalid atomic swap id".to_string())
+            }
+            Err(e) => panic!("unexpected error: {:?}", e),
+        }
+    }
+
+    // Cannot create, no funds
+    let env = mock_env(&sender, &vec![]);
+    let create = CreateMsg {
+        id: "swap0001".to_string(),
+        hash: real_hash(),
+        recipient: "rcpt0001".into(),
+        end_time: 0,
+        end_height: 123456,
+    };
+    let res: HandleResult = handle(&mut deps, env, HandleMsg::Create(create.clone()));
+    match res {
+        Ok(_) => panic!("expected error"),
+        Err(StdError::GenericErr { msg, .. }) => {
+            assert_eq!(msg, "Send some coins to create an atomic swap".to_string())
+        }
+        Err(e) => panic!("unexpected error: {:?}", e),
+    }
+
+    // Cannot create, expired
+    let env = mock_env(&sender, &balance);
+    let create = CreateMsg {
+        id: "swap0001".to_string(),
+        hash: real_hash(),
+        recipient: "rcpt0001".into(),
+        end_height: 0,
+        end_time: 1,
+    };
+    let res: HandleResult = handle(&mut deps, env, HandleMsg::Create(create.clone()));
+    match res {
+        Ok(_) => panic!("expected error"),
+        Err(StdError::GenericErr { msg, .. }) => assert_eq!(msg, "Expired atomic swap".to_string()),
+        Err(e) => panic!("unexpected error: {:?}", e),
+    }
+
+    // Cannot create, invalid hash
+    let env = mock_env(&sender, &balance);
+    let create = CreateMsg {
+        id: "swap0001".to_string(),
+        hash: "bu115h17".to_string(),
+        recipient: "rcpt0001".into(),
+        end_time: 0,
+        end_height: 123456,
+    };
+    let res: HandleResult = handle(&mut deps, env, HandleMsg::Create(create.clone()));
+    match res {
+        Ok(_) => panic!("expected error"),
+        Err(StdError::GenericErr { msg, .. }) => assert_eq!(
+            msg,
+            "Error parsing hash: Invalid character \'u\' at position 1".to_string()
+        ),
+        Err(e) => panic!("unexpected error: {:?}", e),
+    }
+
+    // Can create, all valid
+    let env = mock_env(&sender, &balance);
+    let create = CreateMsg {
+        id: "swap0001".to_string(),
+        hash: real_hash(),
+        recipient: "rcpt0001".into(),
+        end_time: 0,
+        end_height: 123456,
+    };
+    let res: HandleResponse = handle(&mut deps, env, HandleMsg::Create(create.clone())).unwrap();
+    assert_eq!(0, res.messages.len());
+    assert_eq!(log("action", "create"), res.log[0]);
+
+    // Cannot re-create (modify), already existing
+    let new_balance = coins(1, "tokens");
+    let env = mock_env(&sender, &new_balance);
+    let create = CreateMsg {
+        id: "swap0001".to_string(),
+        hash: real_hash(),
+        recipient: "rcpt0001".into(),
+        end_time: 0,
+        end_height: 123456,
+    };
+    let res: HandleResult = handle(&mut deps, env, HandleMsg::Create(create.clone()));
+    match res {
+        Ok(_) => panic!("expected error"),
+        Err(StdError::GenericErr { msg, .. }) => {
+            assert_eq!(msg, "Atomic swap already exists".to_string())
+        }
+        Err(e) => panic!("unexpected error: {:?}", e),
+    }
 }
