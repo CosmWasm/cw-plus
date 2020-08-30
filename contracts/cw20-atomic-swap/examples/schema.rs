2001654aed6c5c9cd862e30aec94796d9e9bf4cf
Mauro Lacy <mauro@lacy.com.es>
2020-08-30 21:45:34 +0200

    Removed unneeded with_title from schema generation

--- contracts/cw20-atomic-swap/examples/schema.rs
+++ contracts/cw20-atomic-swap/examples/schema.rs
@@ -16,7 +16,7 @@ fn main() {
     remove_schemas(&out_dir).unwrap();
 
     export_schema(&schema_for!(InitMsg), &out_dir);
-    export_schema_with_title(&mut schema_for!(HandleMsg), &out_dir, "HandleMsg");
+    export_schema(&schema_for!(HandleMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(ListResponse), &out_dir);
     export_schema(&schema_for!(DetailsResponse), &out_dir);
