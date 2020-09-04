bc9e5eb878917555aad1e6bda795aaf218cd711c
orkunkl <kulceorkun@gmail.com>
2020-09-04 14:01:10 +0300

    cw0: implement display for expiration

--- packages/cw0/src/expiration.rs
+++ packages/cw0/src/expiration.rs
@@ -2,6 +2,7 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::BlockInfo;
+use std::fmt;
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
 #[serde(rename_all = "snake_case")]
@@ -12,6 +13,16 @@ pub enum Expiration {
     AtTime(u64),
     /// Never will never expire. Used to express the empty variant
     Never {},
+}
+
+impl fmt::Display for Expiration {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Expiration::AtHeight(height) => write!(f, "expiration height: {}", height),
+            Expiration::AtTime(time) => write!(f, "expiration time: {}", time),
+            Expiration::Never {} => write!(f, "expiration: never")
+        }
+    }
 }
 
 /// The default (empty value) is to never expire
