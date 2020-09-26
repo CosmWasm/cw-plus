dc97e22429c2b96cc2bad1d8b17690b53a642d1e
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-09-26 12:42:48 +0200

    build schema files

--- contracts/cw721-base/examples/schema.rs
+++ contracts/cw721-base/examples/schema.rs
@@ -15,9 +15,9 @@ fn main() {
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
 
+    export_schema(&schema_for!(InitMsg), &out_dir);
     export_schema(&schema_for!(HandleMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
-    export_schema(&schema_for!(ReceiveMsg), &out_dir);
     export_schema(&schema_for!(AllNftInfoResponse), &out_dir);
     export_schema(&schema_for!(ApprovedForAllResponse), &out_dir);
     export_schema(&schema_for!(ContractInfoResponse), &out_dir);
