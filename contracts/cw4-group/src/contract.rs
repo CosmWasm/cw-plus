4e88cd79df71957e97e9a11e5792fd526dd7f147
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-10-17 17:30:53 +0200

    Logic writen and compiles

--- contracts/cw4-group/src/contract.rs
+++ contracts/cw4-group/src/contract.rs
@@ -1,10 +1,11 @@
 use cosmwasm_std::{
     to_binary, Api, Binary, CanonicalAddr, Env, Extern, HandleResponse, HumanAddr, InitResponse,
-    MessageInfo, Querier, StdResult, Storage,
+    MessageInfo, Order, Querier, StdResult, Storage,
 };
 use cw0::maybe_canonical;
 use cw2::set_contract_version;
 use cw4::{AdminResponse, Member, MemberListResponse, MemberResponse, TotalWeightResponse};
+use cw_storage_plus::Bound;
 
 use crate::error::ContractError;
 use crate::msg::{HandleMsg, InitMsg, QueryMsg};
@@ -57,8 +58,7 @@ pub fn handle<S: Storage, A: Api, Q: Querier>(
 ) -> Result<HandleResponse, ContractError> {
     match msg {
         HandleMsg::UpdateAdmin { admin } => handle_update_admin(deps, info, admin),
-        HandleMsg::UpdateMembers { remove, add } => unimplemented!(),
-        // HandleMsg::UpdateMembers { remove, add } => handle_update_members(deps, info, remove, add),
+        HandleMsg::UpdateMembers { add, remove } => handle_update_members(deps, info, add, remove),
     }
 }
 
@@ -79,16 +79,67 @@ pub fn update_admin<S: Storage, A: Api, Q: Querier>(
 ) -> Result<Option<CanonicalAddr>, ContractError> {
     let api = deps.api;
     ADMIN.update(&mut deps.storage, |state| -> Result<_, ContractError> {
-        let owner = match state {
-            Some(x) => x,
-            None => return Err(ContractError::Unauthorized {}),
-        };
-        if api.canonical_address(&sender)? != owner {
-            return Err(ContractError::Unauthorized {});
-        }
+        assert_admin(api, sender, state)?;
         let new_admin = maybe_canonical(api, new_admin)?;
         Ok(new_admin)
     })
+}
+
+pub fn handle_update_members<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>,
+    info: MessageInfo,
+    add: Vec<Member>,
+    remove: Vec<HumanAddr>,
+) -> Result<HandleResponse, ContractError> {
+    update_members(deps, info.sender, add, remove)?;
+    Ok(HandleResponse::default())
+}
+
+// the logic from handle_update_admin extracted for easier import
+pub fn update_members<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>,
+    sender: HumanAddr,
+    to_add: Vec<Member>,
+    to_remove: Vec<HumanAddr>,
+) -> Result<(), ContractError> {
+    let admin = ADMIN.load(&deps.storage)?;
+    assert_admin(deps.api, sender, admin)?;
+
+    let mut total = TOTAL.load(&deps.storage)?;
+
+    // add all new members and update total
+    for add in to_add.into_iter() {
+        let raw = deps.api.canonical_address(&add.addr)?;
+        MEMBERS.update(&mut deps.storage, &raw, |old| -> StdResult<_> {
+            total -= old.unwrap_or_default();
+            total += add.weight;
+            Ok(add.weight)
+        })?;
+    }
+
+    for remove in to_remove.into_iter() {
+        let raw = deps.api.canonical_address(&remove)?;
+        total -= MEMBERS.may_load(&deps.storage, &raw)?.unwrap_or_default();
+        MEMBERS.remove(&mut deps.storage, &raw);
+    }
+
+    Ok(())
+}
+
+fn assert_admin<A: Api>(
+    api: A,
+    sender: HumanAddr,
+    admin: Option<CanonicalAddr>,
+) -> Result<(), ContractError> {
+    let owner = match admin {
+        Some(x) => x,
+        None => return Err(ContractError::Unauthorized {}),
+    };
+    if api.canonical_address(&sender)? != owner {
+        Err(ContractError::Unauthorized {})
+    } else {
+        Ok(())
+    }
 }
 
 pub fn query<S: Storage, A: Api, Q: Querier>(
@@ -97,15 +148,65 @@ pub fn query<S: Storage, A: Api, Q: Querier>(
     msg: QueryMsg,
 ) -> StdResult<Binary> {
     match msg {
-        _ => unimplemented!(),
-        // QueryMsg::GetCount {} => to_binary(&query_count(deps)?),
+        QueryMsg::Member { addr } => to_binary(&query_member(deps, addr)?),
+        QueryMsg::ListMembers { start_after, limit } => {
+            to_binary(&list_members(deps, start_after, limit)?)
+        }
+        QueryMsg::Admin {} => to_binary(&query_admin(deps)?),
+        QueryMsg::TotalWeight {} => to_binary(&query_total_weight(deps)?),
     }
 }
-//
-// fn query_count<S: Storage, A: Api, Q: Querier>(deps: &Extern<S, A, Q>) -> StdResult<CountResponse> {
-//     let state = CONFIG.load(&deps.storage)?;
-//     Ok(CountResponse { count: state.count })
-// }
+
+fn query_admin<S: Storage, A: Api, Q: Querier>(deps: &Extern<S, A, Q>) -> StdResult<AdminResponse> {
+    let canon = ADMIN.load(&deps.storage)?;
+    let admin = canon.map(|c| deps.api.human_address(&c)).transpose()?;
+    Ok(AdminResponse { admin })
+}
+
+fn query_total_weight<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+) -> StdResult<TotalWeightResponse> {
+    let weight = TOTAL.load(&deps.storage)?;
+    Ok(TotalWeightResponse { weight })
+}
+
+fn query_member<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+    addr: HumanAddr,
+) -> StdResult<MemberResponse> {
+    let raw = deps.api.canonical_address(&addr)?;
+    let weight = MEMBERS.may_load(&deps.storage, &raw)?;
+    Ok(MemberResponse { weight })
+}
+
+// settings for pagination
+const MAX_LIMIT: u32 = 30;
+const DEFAULT_LIMIT: u32 = 10;
+
+fn list_members<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+    start_after: Option<HumanAddr>,
+    limit: Option<u32>,
+) -> StdResult<MemberListResponse> {
+    let limit = limit.unwrap_or(DEFAULT_LIMIT).min(MAX_LIMIT) as usize;
+    let canon = maybe_canonical(deps.api, start_after)?;
+    let start = canon.map(Bound::exclusive);
+
+    let api = &deps.api;
+    let members: StdResult<Vec<_>> = MEMBERS
+        .range(&deps.storage, start, None, Order::Ascending)
+        .take(limit)
+        .map(|item| {
+            let (key, weight) = item?;
+            Ok(Member {
+                addr: api.human_address(&CanonicalAddr::from(key))?,
+                weight,
+            })
+        })
+        .collect();
+
+    Ok(MemberListResponse { members: members? })
+}
 
 #[cfg(test)]
 mod tests {
