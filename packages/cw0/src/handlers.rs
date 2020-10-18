58f0d3f997f92f83a8cb3ff29a7d99d88a03c569
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-10-18 12:58:17 +0200

    Start with router design

--- packages/cw0/src/handlers.rs
+++ packages/cw0/src/handlers.rs
@@ -1,6 +1,9 @@
-use cosmwasm_std::{from_slice, Api, Env, Extern, InitResponse, MessageInfo, Querier, Storage};
+#![allow(dead_code)]
+
+use cosmwasm_std::{from_slice, Api, Env, Extern, HandleResponse, MessageInfo, Querier, Storage};
 use serde::de::DeserializeOwned;
 use serde::export::PhantomData;
+use std::collections::HashMap;
 
 pub trait Handler<S, A, Q>
 where
@@ -14,7 +17,7 @@ where
         env: Env,
         info: MessageInfo,
         msg: Vec<u8>,
-    ) -> Result<InitResponse, String>;
+    ) -> Result<HandleResponse, String>;
 }
 
 pub struct Contract<S, A, Q, T, E>
@@ -30,7 +33,7 @@ where
         env: Env,
         info: MessageInfo,
         msg: T,
-    ) -> Result<InitResponse, E>,
+    ) -> Result<HandleResponse, E>,
     type_store: PhantomData<S>,
     type_api: PhantomData<A>,
     type_querier: PhantomData<Q>,
@@ -50,9 +53,46 @@ where
         env: Env,
         info: MessageInfo,
         msg: Vec<u8>,
-    ) -> Result<InitResponse, String> {
+    ) -> Result<HandleResponse, String> {
         let msg: T = from_slice(&msg).map_err(|e| e.to_string())?;
         let res = (self.handle_fn)(deps, env, info, msg);
         res.map_err(|e| e.to_string())
+    }
+}
+
+pub struct Router<S, A, Q>
+where
+    S: Storage,
+    A: Api,
+    Q: Querier,
+{
+    handlers: HashMap<usize, Box<dyn Handler<S, A, Q>>>,
+}
+
+impl<S, A, Q> Router<S, A, Q>
+where
+    S: Storage,
+    A: Api,
+    Q: Querier,
+{
+    pub fn add_handler(&mut self, handler: Box<dyn Handler<S, A, Q>>) {
+        let idx = self.handlers.len() + 1;
+        self.handlers.insert(idx, handler);
+    }
+
+    // TODO: deps, env from inside router
+    fn handle(
+        &self,
+        code_id: usize,
+        deps: &mut Extern<S, A, Q>,
+        env: Env,
+        info: MessageInfo,
+        msg: Vec<u8>,
+    ) -> Result<HandleResponse, String> {
+        let handler = self
+            .handlers
+            .get(&code_id)
+            .ok_or_else(|| "Unregistered code id".to_string())?;
+        handler.handle(deps, env, info, msg)
     }
 }
