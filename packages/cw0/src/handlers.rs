0df5f9bf366b086b1019f18de86566d78969de42
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-10-18 13:25:44 +0200

    A bit of cleanup

--- packages/cw0/src/handlers.rs
+++ packages/cw0/src/handlers.rs
@@ -1,14 +1,13 @@
 #![allow(dead_code)]
 
-use cosmwasm_std::testing::MockQuerier;
 use cosmwasm_std::{
     from_slice, Api, BlockInfo, ContractInfo, Env, Extern, HandleResponse, HumanAddr, MessageInfo,
     Querier, Storage,
 };
 use serde::de::DeserializeOwned;
-use serde::export::PhantomData;
 use std::cell::Cell;
 use std::collections::HashMap;
+use std::marker::PhantomData;
 
 pub trait Handler<S, A, Q>
 where
@@ -79,23 +78,23 @@ impl<S: Storage + Default> ContractData<S> {
     }
 }
 
-// TODO: use Q not MockQuerier??
-// We define one exact type of querier in our code
-pub struct Router<S, A>
+pub struct Router<S, A, Q>
 where
     S: Storage + Default,
     A: Api,
+    Q: Querier,
 {
-    handlers: HashMap<usize, Box<dyn Handler<S, A, MockQuerier>>>,
+    handlers: HashMap<usize, Box<dyn Handler<S, A, Q>>>,
     contracts: HashMap<HumanAddr, ContractData<S>>,
     block: BlockInfo,
     api: A,
 }
 
-impl<S, A> Router<S, A>
+impl<S, A, Q> Router<S, A, Q>
 where
     S: Storage + Default,
     A: Api,
+    Q: Querier,
 {
     // TODO: mock helper for the test defaults
     pub fn new(api: A, block: BlockInfo) -> Self {
@@ -116,7 +115,7 @@ where
         action(&mut self.block);
     }
 
-    pub fn add_handler(&mut self, handler: Box<dyn Handler<S, A, MockQuerier>>) {
+    pub fn add_handler(&mut self, handler: Box<dyn Handler<S, A, Q>>) {
         let idx = self.handlers.len() + 1;
         self.handlers.insert(idx, handler);
     }
@@ -133,10 +132,11 @@ where
         Ok(addr)
     }
 
-    // TODO: deps from inside router
+    // TODO: where do we create the querier?
     fn handle(
         &self,
         address: HumanAddr,
+        querier: Q,
         info: MessageInfo,
         msg: Vec<u8>,
     ) -> Result<HandleResponse, String> {
@@ -154,7 +154,7 @@ where
         let mut deps = Extern {
             storage,
             api: self.api,
-            querier: MockQuerier::new(&[(&address, &[])]),
+            querier,
         };
         let env = Env {
             block: self.block.clone(),
