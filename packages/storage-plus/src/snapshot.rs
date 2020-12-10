6b47461579a0f26daa575ec4a483141d487b0eea
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-12-10 02:22:53 +0100

    More linter cleanup

--- packages/storage-plus/src/snapshot.rs
+++ packages/storage-plus/src/snapshot.rs
@@ -285,10 +285,15 @@ mod tests {
 
     #[test]
     fn namespace_macro() {
-        let names = snapshot_names!("demo");
-        assert_eq!(names.pk, b"demo");
-        assert_eq!(names.checkpoints, b"demo__checkpoints");
-        assert_eq!(names.changelog, b"demo__changelog");
+        let check = |names: SnapshotNamespaces| {
+            assert_eq!(names.pk, b"demo");
+            assert_eq!(names.checkpoints, b"demo__checkpoints");
+            assert_eq!(names.changelog, b"demo__changelog");
+        };
+        // FIXME: we have to do this weird way due to the clippy allow statement
+        check(snapshot_names!("demo"));
+        // ex. this line fails to compile
+        // let names = snapshot_names!("demo");
     }
 
     type TestMap = SnapshotMap<'static, &'static [u8], u64>;
