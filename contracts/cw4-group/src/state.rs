7e42a95c86c181e025d5ec1c2a0427af1a907cee
Mauro Lacy <mauro@lacy.com.es>
2020-12-11 10:48:42 +0100

    Build const MEMBERS snapshot map with explicit static names

--- contracts/cw4-group/src/state.rs
+++ contracts/cw4-group/src/state.rs
@@ -1,13 +1,15 @@
 use cosmwasm_std::{CanonicalAddr, HumanAddr};
 use cw4::TOTAL_KEY;
-use cw_storage_plus::{snapshot_names, Item, SnapshotMap, SnapshotNamespaces, Strategy};
+use cw_storage_plus::{Item, SnapshotMap, SnapshotNamespaces, Strategy};
 
 pub const ADMIN: Item<Option<CanonicalAddr>> = Item::new(b"admin");
 pub const TOTAL: Item<u64> = Item::new(TOTAL_KEY);
 
 // Note: this must be same as cw4::MEMBERS_KEY but macro needs literal, not const
-pub const MEMBERS: SnapshotMap<&[u8], u64> =
-    SnapshotMap::new(snapshot_names!("members"), Strategy::EveryBlock);
+pub const MEMBERS: SnapshotMap<&[u8], u64> = SnapshotMap::new(
+    SnapshotNamespaces::new(cw4::MEMBERS_KEY, cw4::MEMBERS_CHECK, cw4::MEMBERS_CHANGE),
+    Strategy::EveryBlock,
+);
 
 // store all hook addresses in one item. We cannot have many of them before the contract
 // becomes unusable
