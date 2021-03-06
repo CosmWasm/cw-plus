413b19666f100422e63d5471f44e7b6b9f04b0ae
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-03-06 16:09:20 +0100

    Start converting cw20-base to storage-plus

--- contracts/cw20-base/src/state.rs
+++ contracts/cw20-base/src/state.rs
@@ -2,6 +2,8 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::{CanonicalAddr, Storage, Uint128};
+use cw_storage_plus::{Item, Map};
+
 use cosmwasm_storage::{
     bucket, bucket_read, singleton, singleton_read, Bucket, ReadonlyBucket,
     ReadonlyPrefixedStorage, ReadonlySingleton, Singleton,
@@ -31,46 +33,6 @@ impl TokenInfo {
     }
 }
 
-const TOKEN_INFO_KEY: &[u8] = b"token_info";
-const PREFIX_BALANCE: &[u8] = b"balance";
-const PREFIX_ALLOWANCE: &[u8] = b"allowance";
-
-// meta is the token definition as well as the total_supply
-pub fn token_info(storage: &mut dyn Storage) -> Singleton<TokenInfo> {
-    singleton(storage, TOKEN_INFO_KEY)
-}
-
-pub fn token_info_read(storage: &dyn Storage) -> ReadonlySingleton<TokenInfo> {
-    singleton_read(storage, TOKEN_INFO_KEY)
-}
-
-/// balances are state of the erc20 tokens
-pub fn balances(storage: &mut dyn Storage) -> Bucket<Uint128> {
-    bucket(storage, PREFIX_BALANCE)
-}
-
-/// balances are state of the erc20 tokens (read-only version for queries)
-pub fn balances_read(storage: &dyn Storage) -> ReadonlyBucket<Uint128> {
-    bucket_read(storage, PREFIX_BALANCE)
-}
-
-pub fn balances_prefix_read(storage: &dyn Storage) -> ReadonlyPrefixedStorage {
-    ReadonlyPrefixedStorage::new(storage, PREFIX_BALANCE)
-}
-
-/// returns a bucket with all allowances authorized by this owner (query it by spender)
-pub fn allowances<'a>(
-    storage: &'a mut dyn Storage,
-    owner: &CanonicalAddr,
-) -> Bucket<'a, AllowanceResponse> {
-    Bucket::multilevel(storage, &[PREFIX_ALLOWANCE, owner.as_slice()])
-}
-
-/// returns a bucket with all allowances authorized by this owner (query it by spender)
-/// (read-only version for queries)
-pub fn allowances_read<'a>(
-    storage: &'a dyn Storage,
-    owner: &CanonicalAddr,
-) -> ReadonlyBucket<'a, AllowanceResponse> {
-    ReadonlyBucket::multilevel(storage, &[PREFIX_ALLOWANCE, owner.as_slice()])
-}
+const TOKEN_INFO: Item<TokenInfo> = Item::new("token_info");
+const BALANCES: Map<&[u8], Uint128> = Map::new("balance");
+const ALLOWANCES: Map<(&[u8], &[u8]), AllowanceResponse> = Map::new("allowance");
