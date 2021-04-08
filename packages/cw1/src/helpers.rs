714049745ac9e21053c6654a4361cac6684858f9
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-04-08 21:28:35 +0200

    Remove Canonical helpers

--- packages/cw1/src/helpers.rs
+++ packages/cw1/src/helpers.rs
@@ -1,7 +1,7 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{to_binary, Addr, Api, CanonicalAddr, CosmosMsg, StdResult, WasmMsg};
+use cosmwasm_std::{to_binary, Addr, CosmosMsg, StdResult, WasmMsg};
 
 use crate::msg::Cw1ExecuteMsg;
 
@@ -17,12 +17,6 @@ impl Cw1Contract {
         self.0.clone()
     }
 
-    /// Convert this address to a form fit for storage
-    pub fn canonical<A: Api>(&self, api: &A) -> StdResult<Cw1CanonicalContract> {
-        let canon = api.addr_canonicalize(self.0.as_ref())?;
-        Ok(Cw1CanonicalContract(canon))
-    }
-
     pub fn execute<T: Into<Vec<CosmosMsg>>>(&self, msgs: T) -> StdResult<CosmosMsg> {
         let msg = Cw1ExecuteMsg::Execute { msgs: msgs.into() };
         Ok(WasmMsg::Execute {
@@ -31,18 +25,5 @@ impl Cw1Contract {
             send: vec![],
         }
         .into())
-    }
-}
-
-/// This is a respresentation of Cw1Contract for storage.
-/// Don't use it directly, just translate to the Cw1Contract when needed.
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct Cw1CanonicalContract(pub CanonicalAddr);
-
-impl Cw1CanonicalContract {
-    /// Convert this address to a form fit for usage in messages and queries
-    pub fn human<A: Api>(&self, api: &A) -> StdResult<Cw1Contract> {
-        let human = api.addr_humanize(&self.0)?;
-        Ok(Cw1Contract(human))
     }
 }
