0c08564b47522e55b1b6cb7f3e777754a7f8b8f5
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-04-08 16:41:50 +0200

    Update cw1-whitelist

--- contracts/cw1-whitelist/src/state.rs
+++ contracts/cw1-whitelist/src/state.rs
@@ -1,23 +1,23 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::CanonicalAddr;
+use cosmwasm_std::Addr;
 use cw_storage_plus::Item;
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug, Default)]
 pub struct AdminList {
-    pub admins: Vec<CanonicalAddr>,
+    pub admins: Vec<Addr>,
     pub mutable: bool,
 }
 
 impl AdminList {
     /// returns true if the address is a registered admin
-    pub fn is_admin(&self, addr: &CanonicalAddr) -> bool {
-        self.admins.iter().any(|a| a == addr)
+    pub fn is_admin(&self, addr: &str) -> bool {
+        self.admins.iter().any(|a| a.as_ref() == addr)
     }
 
     /// returns true if the address is a registered admin and the config is mutable
-    pub fn can_modify(&self, addr: &CanonicalAddr) -> bool {
+    pub fn can_modify(&self, addr: &str) -> bool {
         self.mutable && self.is_admin(addr)
     }
 }
@@ -27,47 +27,42 @@ pub const ADMIN_LIST: Item<AdminList> = Item::new("admin_list");
 #[cfg(test)]
 mod tests {
     use super::*;
-    use cosmwasm_std::testing::MockApi;
-    use cosmwasm_std::{Api, HumanAddr};
 
     #[test]
     fn is_admin() {
-        let api = MockApi::default();
         let admins: Vec<_> = vec!["bob", "paul", "john"]
             .into_iter()
-            .map(|name| api.canonical_address(&HumanAddr::from(name)).unwrap())
+            .map(Addr::unchecked)
             .collect();
         let config = AdminList {
             admins: admins.clone(),
             mutable: false,
         };
 
-        assert!(config.is_admin(&admins[0]));
-        assert!(config.is_admin(&admins[2]));
-        let other = api.canonical_address(&HumanAddr::from("other")).unwrap();
-        assert!(!config.is_admin(&other));
+        assert!(config.is_admin(admins[0].as_ref()));
+        assert!(config.is_admin(admins[2].as_ref()));
+        assert!(!config.is_admin("other"));
     }
 
     #[test]
     fn can_modify() {
-        let api = MockApi::default();
-        let alice = api.canonical_address(&HumanAddr::from("alice")).unwrap();
-        let bob = api.canonical_address(&HumanAddr::from("bob")).unwrap();
+        let alice = Addr::unchecked("alice");
+        let bob = Addr::unchecked("bob");
 
         // admin can modify mutable contract
         let config = AdminList {
             admins: vec![bob.clone()],
             mutable: true,
         };
-        assert!(!config.can_modify(&alice));
-        assert!(config.can_modify(&bob));
+        assert!(!config.can_modify(alice.as_ref()));
+        assert!(config.can_modify(bob.as_ref()));
 
         // no one can modify an immutable contract
         let config = AdminList {
             admins: vec![alice.clone()],
             mutable: false,
         };
-        assert!(!config.can_modify(&alice));
-        assert!(!config.can_modify(&bob));
+        assert!(!config.can_modify(alice.as_ref()));
+        assert!(!config.can_modify(bob.as_ref()));
     }
 }
