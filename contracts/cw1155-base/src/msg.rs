cddeafd74b9bd89f3498a3f63101cd50c292fd96
Mauro Lacy <mauro@lacy.com.es>
2021-04-11 12:43:30 +0200

    Use String form msg

--- contracts/cw1155-base/src/msg.rs
+++ contracts/cw1155-base/src/msg.rs
@@ -1,4 +1,3 @@
-use cosmwasm_std::Addr;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -7,5 +6,5 @@ pub struct InitMsg {
     /// The minter is the only one who can create new tokens.
     /// This is designed for a base token platform that is controlled by an external program or
     /// contract.
-    pub minter: Addr,
+    pub minter: String,
 }
