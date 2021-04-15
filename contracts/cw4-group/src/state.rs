27a99364e77cb099bfac056a5a1ccbf9c25009f1
Mauro Lacy <mauro@lacy.com.es>
2021-04-15 22:16:05 +0200

    Add members() IndexedSnapshotMap

--- contracts/cw4-group/src/state.rs
+++ contracts/cw4-group/src/state.rs
@@ -1,7 +1,9 @@
 use cosmwasm_std::Addr;
 use cw4::TOTAL_KEY;
 use cw_controllers::{Admin, Hooks};
-use cw_storage_plus::{Item, SnapshotMap, Strategy};
+use cw_storage_plus::{
+    Index, IndexList, IndexedSnapshotMap, Item, MultiIndex, PkOwned, SnapshotMap, Strategy, U64Key,
+};
 
 pub const ADMIN: Admin = Admin::new("admin");
 pub const HOOKS: Hooks = Hooks::new("cw4-hooks");
@@ -14,3 +16,32 @@ pub const MEMBERS: SnapshotMap<&Addr, u64> = SnapshotMap::new(
     cw4::MEMBERS_CHANGELOG,
     Strategy::EveryBlock,
 );
+
+pub struct MemberIndexes<'a> {
+    // pk goes to second tuple element
+    pub weight: MultiIndex<'a, (U64Key, PkOwned), u64>,
+}
+
+impl<'a> IndexList<u64> for MemberIndexes<'a> {
+    fn get_indexes(&'_ self) -> Box<dyn Iterator<Item = &'_ dyn Index<u64>> + '_> {
+        let v: Vec<&dyn Index<u64>> = vec![&self.weight];
+        Box::new(v.into_iter())
+    }
+}
+
+pub fn members<'a>() -> IndexedSnapshotMap<'a, &'a Addr, u64, MemberIndexes<'a>> {
+    let indexes = MemberIndexes {
+        weight: MultiIndex::new(
+            |&w, k| (U64Key::new(w), PkOwned(k)),
+            cw4::MEMBERS_KEY,
+            "members__weight",
+        ),
+    };
+    IndexedSnapshotMap::new(
+        cw4::MEMBERS_KEY,
+        cw4::MEMBERS_CHECKPOINTS,
+        cw4::MEMBERS_CHANGELOG,
+        Strategy::EveryBlock,
+        indexes,
+    )
+}
