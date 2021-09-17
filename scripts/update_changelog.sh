fc65261605d8048cb5a66ff6ab419962b0e51a5e
Mauro Lacy <mauro@lacy.com.es>
2021-09-17 11:55:09 +0200

    Add --since-tag parameter / latest tag detection

--- scripts/update_changelog.sh
+++ scripts/update_changelog.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
 
-github_changelog_generator -u CosmWasm -p cw-plus --base CHANGELOG.md $@
+
+ORIGINAL_OPTS="$@"
+OPTS=$(getopt -l "help,since-tag:,latest-tag" -o "hl" -- "$@") || exit 1
+
+eval set -- "$OPTS"
+while true
+do
+case $1 in
+  -h|--help)
+    echo -e "Usage: $0 [--since-tag <tag>]\n
+    -h, --help          Display help\n
+    --since-tag <tag>   Process changes since tag <tag>\r
+    -l, --latest-tag    Process changes since latest tag"
+    exit 0
+    ;;
+--since-tag)
+    shift
+    TAG="$1"
+    ;;
+-l|--latest-tag)
+    TAG=$(git tag --sort=creatordate | egrep '^v[0-9]+\.[0-9]+\.[0-9]+' | tail -1)
+    ORIGINAL_OPTS="--since-tag $TAG"
+    ;;
+--)
+    shift
+    break
+    ;;
+esac
+shift
+done
+
+cp CHANGELOG.md /tmp/CHANGELOG.md.$$
+sed -i -n "/^## \[$TAG\]/,\$p" CHANGELOG.md
+
+github_changelog_generator -u CosmWasm -p cw-plus --base CHANGELOG.md $ORIGINAL_OPTS || cp /tmp/CHANGELOG.md.$$ CHANGELOG.md
+
+rm -f /tmp/CHANGELOG.md.$$
