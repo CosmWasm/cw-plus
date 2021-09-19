da84d938432a0159b987ed049080f30f497304e8
Mauro Lacy <mauro@lacy.com.es>
2021-09-19 21:14:11 +0200

    Remove obsolete iterator feature attr

--- packages/storage-plus/src/map.rs
+++ packages/storage-plus/src/map.rs
@@ -42,17 +42,14 @@ where
         Path::new(self.namespace, &k.key())
     }
 
-    #[cfg(feature = "iterator")]
     pub fn prefix(&self, p: K::Prefix) -> Prefix<T> {
         Prefix::new(self.namespace, &p.prefix())
     }
 
-    #[cfg(feature = "iterator")]
     pub fn sub_prefix(&self, p: K::SubPrefix) -> Prefix<T> {
         Prefix::new(self.namespace, &p.prefix())
     }
 
-    #[cfg(feature = "iterator")]
     pub(crate) fn no_prefix(&self) -> Prefix<T> {
         Prefix::new(self.namespace, &[])
     }
@@ -132,7 +129,6 @@ where
 }
 
 // short-cut for simple keys, rather than .prefix(()).range(...)
-#[cfg(feature = "iterator")]
 impl<'a, K, T> Map<'a, K, T>
 where
     T: Serialize + DeserializeOwned,
@@ -228,13 +224,11 @@ mod test {
     use serde::{Deserialize, Serialize};
     use std::ops::Deref;
 
-    #[cfg(feature = "iterator")]
     use crate::iter_helpers::to_length_prefixed;
     #[cfg(feature = "iterator")]
     use crate::U32Key;
     use crate::U8Key;
     use cosmwasm_std::testing::MockStorage;
-    #[cfg(feature = "iterator")]
     use cosmwasm_std::{Order, StdResult};
 
     #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
@@ -379,7 +373,6 @@ mod test {
     }
 
     #[test]
-    #[cfg(feature = "iterator")]
     fn range_simple_key() {
         let mut store = MockStorage::new();
 
@@ -883,7 +876,6 @@ mod test {
     }
 
     #[test]
-    #[cfg(feature = "iterator")]
     fn readme_with_range() -> StdResult<()> {
         let mut store = MockStorage::new();
 
