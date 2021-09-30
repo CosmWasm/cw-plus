c614ac49e6c23fcc6246d94636e67f6dc7a5e05c
Mauro Lacy <mauro@lacy.com.es>
2021-09-30 12:35:08 +0200

    Remove FIXME?

--- packages/storage-plus/src/de.rs
+++ packages/storage-plus/src/de.rs
@@ -37,7 +37,6 @@ macro_rules! string_de {
             type Output = String;
 
             fn from_slice(value: &[u8]) -> StdResult<Self::Output> {
-                // FIXME?: Use `from_utf8_unchecked` for String, &str
                 String::from_utf8(value.to_vec())
                     // FIXME: Add and use StdError utf-8 error From helper
                     .map_err(|err| StdError::generic_err(err.to_string()))
