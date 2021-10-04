549c2b5673d6848f0dd56324d8d8429f89608e1b
Mauro Lacy <mauro@lacy.com.es>
2021-10-04 21:29:27 +0200

    Add parse_length helper

--- packages/storage-plus/src/de.rs
+++ packages/storage-plus/src/de.rs
@@ -121,18 +121,22 @@ impl KeyDeserialize for TimestampKey {
     }
 }
 
+fn parse_length(value: &[u8]) -> StdResult<usize> {
+    Ok(u16::from_be_bytes(
+        value
+            .try_into()
+            .map_err(|_| StdError::generic_err("Could not read 2 byte length"))?,
+    )
+    .into())
+}
+
 impl<T: KeyDeserialize, U: KeyDeserialize> KeyDeserialize for (T, U) {
     type Output = (T::Output, U::Output);
 
     #[inline(always)]
     fn from_vec(mut value: Vec<u8>) -> StdResult<Self::Output> {
         let mut tu = value.split_off(2);
-        let t_len = u16::from_be_bytes(
-            value
-                .as_slice()
-                .try_into()
-                .map_err(|err: TryFromSliceError| StdError::generic_err(err.to_string()))?,
-        ) as usize;
+        let t_len = parse_length(&value)?;
         let u = tu.split_off(t_len);
 
         Ok((T::from_vec(tu)?, U::from_vec(u)?))
@@ -145,21 +149,11 @@ impl<T: KeyDeserialize, U: KeyDeserialize, V: KeyDeserialize> KeyDeserialize for
     #[inline(always)]
     fn from_vec(mut value: Vec<u8>) -> StdResult<Self::Output> {
         let mut tuv = value.split_off(2);
-        let t_len = u16::from_be_bytes(
-            value
-                .as_slice()
-                .try_into()
-                .map_err(|err: TryFromSliceError| StdError::generic_err(err.to_string()))?,
-        ) as usize;
+        let t_len = parse_length(&value)?;
         let mut len_uv = tuv.split_off(t_len);
 
         let mut uv = len_uv.split_off(2);
-        let u_len = u16::from_be_bytes(
-            len_uv
-                .as_slice()
-                .try_into()
-                .map_err(|err: TryFromSliceError| StdError::generic_err(err.to_string()))?,
-        ) as usize;
+        let u_len = parse_length(&len_uv)?;
         let v = uv.split_off(u_len);
 
         Ok((T::from_vec(tuv)?, U::from_vec(uv)?, V::from_vec(v)?))
