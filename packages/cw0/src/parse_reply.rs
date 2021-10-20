18ddb76bababab71a093ba83ab076b833cee6d86
Mauro Lacy <mauro@lacy.com.es>
2021-10-20 18:16:06 +0200

    Add manual protobuf decoding

--- packages/cw0/src/parse_reply.rs
+++ packages/cw0/src/parse_reply.rs
@@ -1,35 +1,57 @@
-use prost::Message;
 use thiserror::Error;
 
-use cosmwasm_std::Reply;
+use cosmwasm_std::{Binary, Reply};
 
-#[derive(Clone, PartialEq, Message)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct MsgInstantiateContractResponse {
-    #[prost(string, tag = "1")]
-    pub contract_address: ::prost::alloc::string::String,
-    #[prost(bytes, tag = "2")]
-    pub data: ::prost::alloc::vec::Vec<u8>,
+    pub contract_address: String,
+    pub data: Option<Binary>, // FIXME: Confirm if Option
 }
 
 pub fn parse_reply_instantiate_data(
     msg: Reply,
 ) -> Result<MsgInstantiateContractResponse, ParseReplyError> {
     let id = msg.id;
-    let res: MsgInstantiateContractResponse = Message::decode(
-        msg.result
-            .into_result()
-            .map_err(ParseReplyError::SubMsgFailure)?
-            .data
-            .ok_or_else(|| ParseReplyError::ParseFailure {
-                id,
-                err: "Missing reply data".to_owned(),
-            })?
-            .as_slice(),
-    )
-    .map_err(|err| ParseReplyError::ParseFailure {
-        id,
-        err: err.to_string(),
-    })?;
+    let data = msg
+        .result
+        .into_result()
+        .map_err(ParseReplyError::SubMsgFailure)?
+        .data
+        .ok_or_else(|| ParseReplyError::ParseFailure {
+            id,
+            err: "Missing reply data".to_owned(),
+        })?;
+    // Manual protobuf decoding
+    let data = data.0;
+    println!("reply data 0: {:#?}", data);
+    // FIXME: avoid panics
+    let (wire_type, data) = data.as_slice().split_at(1);
+    println!("reply data 1: {:#?}", data);
+    if wire_type[0] != b'\x0a' {
+        return Err(ParseReplyError::ParseFailure { id, err: "failed to decode Protobuf message: MsgInstantiateContractResponse.contract_address: invalid wire type".to_owned() });
+    }
+    let (len, data) = data.split_at(1);
+    println!("reply data 2: {:#?}", data);
+    let (contract_addr, data) = data.split_at(len[0] as usize);
+    println!("reply data 3: {:#?}", data);
+    let (wire_type, data) = data.split_at(1);
+    if wire_type[0] != b'\x12' {
+        return Err(ParseReplyError::ParseFailure { id, err: "failed to decode Protobuf message: MsgInstantiateContractResponse.data: invalid wire type".to_owned() });
+    }
+    let (len, data) = data.split_at(1);
+    println!("reply data 4: {:#?}", data);
+    let (data, _rest) = data.split_at(len[0] as usize);
+    println!("reply data 5: {:#?}", data);
+    let data = if data.is_empty() {
+        None
+    } else {
+        Some(Binary(data.to_vec()))
+    };
+
+    let res = MsgInstantiateContractResponse {
+        contract_address: String::from_utf8(contract_addr.to_vec())?,
+        data,
+    };
 
     Ok(res)
 }
@@ -41,19 +63,31 @@ pub enum ParseReplyError {
 
     #[error("Invalid reply from sub-message {id}: {err}")]
     ParseFailure { id: u64, err: String },
+
+    #[error("Error occurred while converting from UTF-8")]
+    FromUtf8(#[from] std::string::FromUtf8Error),
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
     use cosmwasm_std::{ContractResult, SubMsgExecutionResponse};
+    use prost::Message;
+
+    #[derive(Clone, PartialEq, Message)]
+    pub struct MsgInstantiateContractResponse {
+        #[prost(string, tag = "1")]
+        pub contract_address: ::prost::alloc::string::String,
+        #[prost(bytes, tag = "2")]
+        pub data: ::prost::alloc::vec::Vec<u8>,
+    }
 
     #[test]
     fn parse_reply_instantiate_data_works() {
         let instantiate_reply_data: &str = "Contract #1";
         let instantiate_reply = MsgInstantiateContractResponse {
             contract_address: instantiate_reply_data.to_string(),
-            data: vec![],
+            data: vec![1u8, 2, 3, 4],
         };
         let mut encoded_instantiate_reply =
             Vec::<u8>::with_capacity(instantiate_reply.encoded_len());
@@ -74,9 +108,9 @@ mod test {
         let res = parse_reply_instantiate_data(msg).unwrap();
         assert_eq!(
             res,
-            MsgInstantiateContractResponse {
+            super::MsgInstantiateContractResponse {
                 contract_address: instantiate_reply_data.into(),
-                data: vec![],
+                data: Some(Binary(vec![1u8, 2, 3, 4])),
             }
         );
     }
