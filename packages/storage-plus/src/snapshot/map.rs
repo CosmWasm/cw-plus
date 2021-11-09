2481dcd30d8515c8a4bb70186cb2cee56bb726e8
Mauro Lacy <mauro@lacy.com.es>
2021-11-09 16:14:10 +0100

    Add prefix_de test

--- packages/storage-plus/src/snapshot/map.rs
+++ packages/storage-plus/src/snapshot/map.rs
@@ -479,4 +479,22 @@ mod tests {
             ]
         );
     }
+
+    #[test]
+    #[cfg(feature = "iterator")]
+    fn prefix_de_composite_key() {
+        use cosmwasm_std::Order;
+
+        let mut store = MockStorage::new();
+        init_data_composite_key(&EVERY_COMPOSITE_KEY, &mut store);
+
+        // let's prefix and iterate
+        let all: StdResult<Vec<_>> = EVERY_COMPOSITE_KEY
+            .prefix_de("C")
+            .range_de(&store, None, None, Order::Ascending)
+            .collect();
+        let all = all.unwrap();
+        assert_eq!(1, all.len());
+        assert_eq!(all, vec![("A".into(), 22),]);
+    }
 }
