3446fb5c637b008422bb1e79d83eb18b7da4fe3c
Bartłomiej Kuras <bart.k@confio.gmbh>
2021-12-06 08:56:35 +0100

    Contract generation basics

---
+++ packages/cw-derive/src/lib.rs
@@ -0,0 +1,184 @@
+use convert_case::{Case, Casing};
+use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
+use syn::parse::{Parse, Parser};
+use syn::spanned::Spanned;
+use syn::{parse_macro_input, Error, FnArg, Ident, ItemTrait, Pat, TraitItem, TraitItemMethod};
+
+mod parser;
+
+/// Macro generating messages from contract trait.
+///
+/// ## Example usage
+/// ```
+/// ! use cosmwasm_std::Response;
+///
+/// ! struct Error;
+///
+/// ! struct Member;
+///
+/// #[cw_derive::interface(module=msg, exec=Execute, query=Query)]
+/// trait Cw4 {
+///     #[msg(exec)]
+///     fn update_admin(&self, ctx: Ctx, admin: Option<String>) -> Result<Response, Error>;
+///
+///     #[msg(exec)]
+///     fn update_members(&self, ctx: Ctx, remove: Vec<String>, add: Vec<Member>)
+///         -> Result<Response, Error>;
+///
+///     #[msg(exec)]
+///     fn add_hook(&self, ctx: Ctx, addr: String) -> Result<Response, Error>;
+///
+///     #[msg(exec)]
+///     fn remove_hook(&self, ctx: Ctx, addr: String) -> Result<Response, Error>;
+/// }
+/// ```
+///
+/// This would generate output like:
+///
+/// ```
+/// mod msg {
+///     #[derive(serde::Serialize, serde::Deserialize, Clone, Debug, PartialEq, schemars::JsonSchema)]
+///     #[serde(rename_all = "snake_case")]
+///     enum Execute {
+///         UpdateAdmin { admin: Option<String> },
+///         UpdateMembers {
+///             remove: Vec<String>,
+///             add: Vec<Member>,
+///         },
+///         AddHook { addr: String },
+///         RemoveHook { addr: String },
+///     }
+/// }
+/// ```
+///
+/// ## Parameters
+///
+/// `interface` attribute takes optional parameters:
+/// * `module` - defines module name, where all generated messages would be encapsulated; no
+/// additional module would be created if not provided
+/// * `exec` - sets name for execution messages type, `ExecMsg` by default
+/// * `query` - sets name for query messages type, `QueryMsg` by default
+///
+/// ## Attributes
+///
+/// Messages structures are generated basing on interface trait method. Some hints for generator
+/// may be provided by additional attributes.
+///
+/// * `msg(msg_type)` - Hints, that this function is a message variant of specific type. Methods
+/// which are not marked with this attribute are ignored by generator. `msg_type` is one of:
+///   * `exec` - this is execute message variant
+#[proc_macro_attribute]
+pub fn interface(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let item = item.clone();
+    let attrs = parse_macro_input!(attr as parser::InterfaceArgs);
+    let input = parse_macro_input!(item as ItemTrait);
+
+    let exec = build_msg(&attrs.exec, &input, parser::InterfaceMsgAttr::Exec);
+    let query = build_msg(&attrs.query, &input, parser::InterfaceMsgAttr::Query);
+
+    let expanded = if let Some(module) = attrs.module {
+        quote! {
+            mod #module {
+                #exec
+
+                #query
+            }
+
+            #input
+        }
+    } else {
+        quote! {
+            #exec
+
+            #query
+
+            #input
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+
+/// Builds message basing on input trait
+fn build_msg(name: &Ident, source: &ItemTrait, ty: parser::InterfaceMsgAttr) -> TokenStream2 {
+    let variants = source.items.iter().filter_map(|item| match item {
+        TraitItem::Method(method) => {
+            let msg_attr = method.attrs.iter().find(|attr| attr.path.is_ident("msg"))?;
+            let attr = match parser::InterfaceMsgAttr::parse.parse2(msg_attr.tokens.clone()) {
+                Ok(attr) => attr,
+                Err(err) => return Some(msg_variant_err(&method, err)),
+            };
+
+            if attr == ty {
+                Some(msg_variant(&method))
+            } else {
+                None
+            }
+        }
+        _ => None,
+    });
+
+    quote! {
+        #[derive(serde::Serialize, serde::Deserialize, Clone, Debug, PartialEq, schemars::JsonSchema)]
+        #[serde(rename_all = "snake_case")]
+        enum #name {
+            #(#variants,)*
+        }
+    }
+}
+
+/// Builds message variant error
+fn msg_variant_err(method: &TraitItemMethod, err: Error) -> TokenStream2 {
+    let variant = &method.sig.ident;
+    let name = variant.to_string().to_case(Case::Camel);
+    let variant = Ident::new(&name, variant.span());
+    let err = err.into_compile_error();
+
+    quote! {
+        #variant(#err)
+    }
+}
+
+/// Builds single message variant from method definition
+fn msg_variant(method: &TraitItemMethod) -> TokenStream2 {
+    let name = &method.sig.ident;
+    let name = Ident::new(&name.to_string().to_case(Case::Camel), name.span());
+
+    let fields = method.sig.inputs.iter().skip(2).map(|arg| match arg {
+        FnArg::Receiver(item) => {
+            let err = Error::new(item.span(), "Unexpected `self` argument").into_compile_error();
+            quote! {
+                _self: #err
+            }
+        }
+        FnArg::Typed(item) => {
+            let name = match &*item.pat {
+                Pat::Path(p) if p.path.get_ident().is_some() => p.path.get_ident().unwrap(),
+                pat => {
+                    // TODO: Support pattern arguments, when decorated with argument with item
+                    // name
+                    let err = Error::new(pat.span(), "Expected argument name, pattern occurred")
+                        .into_compile_error();
+                    return quote! {
+                        _invalid: #err
+                    };
+                }
+            };
+
+            let name = Ident::new(&name.to_string().to_case(Case::Camel), name.span());
+            let ty = &item.ty;
+
+            quote! {
+                #name: #ty
+            }
+        }
+    });
+
+    quote! {
+        #name {
+            #(#fields,)*
+        }
+    }
+}
