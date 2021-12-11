42d5db965f6792d16306d174203d268687fac0cc
Mauro Lacy <mauro@lacy.com.es>
2021-12-11 18:57:57 +0100

    Rename no_prefix to no_prefix_raw, no_prefix_de to no_prefix in IndexedSnapshotMap for consistency

--- packages/storage-plus/src/indexed_snapshot.rs
+++ packages/storage-plus/src/indexed_snapshot.rs
@@ -175,7 +175,7 @@ where
     }
 
     // use no_prefix to scan -> range
-    pub fn no_prefix(&self) -> Prefix<Vec<u8>, T> {
+    pub fn no_prefix_raw(&self) -> Prefix<Vec<u8>, T> {
         Prefix::new(self.pk_namespace, &[])
     }
 }
@@ -199,7 +199,7 @@ where
     where
         T: 'c,
     {
-        self.no_prefix().range_raw(store, min, max, order)
+        self.no_prefix_raw().range_raw(store, min, max, order)
     }
 
     pub fn keys_raw<'c>(
@@ -209,7 +209,7 @@ where
         max: Option<Bound>,
         order: cosmwasm_std::Order,
     ) -> Box<dyn Iterator<Item = Vec<u8>> + 'c> {
-        self.no_prefix().keys_raw(store, min, max, order)
+        self.no_prefix_raw().keys_raw(store, min, max, order)
     }
 }
 
@@ -271,7 +271,7 @@ where
         T: 'c,
         K::Output: 'static,
     {
-        self.no_prefix_de().range(store, min, max, order)
+        self.no_prefix().range(store, min, max, order)
     }
 
     pub fn keys<'c>(
@@ -285,10 +285,10 @@ where
         T: 'c,
         K::Output: 'static,
     {
-        self.no_prefix_de().keys(store, min, max, order)
+        self.no_prefix().keys(store, min, max, order)
     }
 
-    fn no_prefix_de(&self) -> Prefix<K, T> {
+    fn no_prefix(&self) -> Prefix<K, T> {
         Prefix::new(self.pk_namespace, &[])
     }
 }
