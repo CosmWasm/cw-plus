05d185516cb2a374ded016a968e543cecd3a8798
Mauro Lacy <mauro@lacy.com.es>
2021-12-11 07:44:54 +0100

    Replace no_prefix() + range() by no_prefix() + range_raw() in indexes

--- packages/storage-plus/src/indexes/multi.rs
+++ packages/storage-plus/src/indexes/multi.rs
@@ -216,7 +216,7 @@ where
     where
         T: 'c,
     {
-        self.no_prefix().range(store, min, max, order)
+        self.no_prefix().range_raw(store, min, max, order)
     }
 
     pub fn keys<'c>(
