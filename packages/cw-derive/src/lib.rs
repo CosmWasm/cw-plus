d7372fa88533e6ed0501357abec5e335501c2e8a
Bartłomiej Kuras <bart.k@confio.gmbh>
2021-12-16 15:26:58 +0100

    Generating basic structures

--- packages/cw-derive/src/lib.rs
+++ packages/cw-derive/src/lib.rs
@@ -2,21 +2,27 @@ use convert_case::{Case, Casing};
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
+use syn::fold::Fold;
 use syn::parse::{Parse, Parser};
 use syn::spanned::Spanned;
 use syn::{parse_macro_input, Error, FnArg, Ident, ItemTrait, Pat, TraitItem, TraitItemMethod};
 
 mod parser;
+mod strip_input;
+
+use strip_input::StripInput;
 
 /// Macro generating messages from contract trait.
 ///
 /// ## Example usage
 /// ```
-/// ! use cosmwasm_std::Response;
+/// # use cosmwasm_std::Response;
 ///
-/// ! struct Error;
+/// # struct Ctx;
+/// # struct Error;
 ///
-/// ! struct Member;
+/// # #[derive(serde::Serialize, serde::Deserialize, Clone, Debug, PartialEq, schemars::JsonSchema)]
+/// # struct Member;
 ///
 /// #[cw_derive::interface(module=msg, exec=Execute, query=Query)]
 /// trait Cw4 {
@@ -27,11 +33,11 @@ mod parser;
 ///     fn update_members(&self, ctx: Ctx, remove: Vec<String>, add: Vec<Member>)
 ///         -> Result<Response, Error>;
 ///
-///     #[msg(exec)]
-///     fn add_hook(&self, ctx: Ctx, addr: String) -> Result<Response, Error>;
+///     #[msg(query)]
+///     fn admin(&self, ctx: Ctx) -> Result<Response, Error>;
 ///
-///     #[msg(exec)]
-///     fn remove_hook(&self, ctx: Ctx, addr: String) -> Result<Response, Error>;
+///     #[msg(query)]
+///     fn member(&self, ctx: Ctx, addr: String, at_height: Option<u64>) -> Result<Response, Error>;
 /// }
 /// ```
 ///
@@ -39,6 +45,9 @@ mod parser;
 ///
 /// ```
 /// pub mod msg {
+///     # #[derive(serde::Serialize, serde::Deserialize, Clone, Debug, PartialEq, schemars::JsonSchema)]
+///     # struct Member;
+///
 ///     #[derive(serde::Serialize, serde::Deserialize, Clone, Debug, PartialEq, schemars::JsonSchema)]
 ///     #[serde(rename_all = "snake_case")]
 ///     pub enum Execute {
@@ -79,9 +88,12 @@ pub fn interface(attr: TokenStream, item: TokenStream) -> TokenStream {
     let exec = build_msg(&attrs.exec, &input, parser::InterfaceMsgAttr::Exec);
     let query = build_msg(&attrs.query, &input, parser::InterfaceMsgAttr::Query);
 
+    let input = StripInput.fold_item_trait(input);
+
     let expanded = if let Some(module) = attrs.module {
         quote! {
             pub mod #module {
+                use super::*;
                 #exec
 
                 #query
@@ -145,37 +157,64 @@ fn msg_variant_err(method: &TraitItemMethod, err: Error) -> TokenStream2 {
 /// Builds single message variant from method definition
 fn msg_variant(method: &TraitItemMethod) -> TokenStream2 {
     let name = &method.sig.ident;
-    let name = Ident::new(&name.to_string().to_case(Case::Camel), name.span());
+    let name = Ident::new(&name.to_string().to_case(Case::UpperCamel), name.span());
 
-    let fields = method.sig.inputs.iter().skip(2).map(|arg| match arg {
-        FnArg::Receiver(item) => {
-            let err = Error::new(item.span(), "Unexpected `self` argument").into_compile_error();
-            quote! {
-                _self: #err
-            }
-        }
-        FnArg::Typed(item) => {
-            let name = match &*item.pat {
-                Pat::Path(p) if p.path.get_ident().is_some() => p.path.get_ident().unwrap(),
-                pat => {
-                    // TODO: Support pattern arguments, when decorated with argument with item
-                    // name
-                    let err = Error::new(pat.span(), "Expected argument name, pattern occurred")
-                        .into_compile_error();
-                    return quote! {
-                        _invalid: #err
-                    };
+    let fields = method
+        .sig
+        .inputs
+        .iter()
+        .skip(2)
+        .enumerate()
+        .map(|(idx, arg)| {
+            match arg {
+                FnArg::Receiver(item) => {
+                    let err =
+                        Error::new(item.span(), "Unexpected `self` argument").into_compile_error();
+                    quote! {
+                        _self: #err
+                    }
                 }
-            };
+                FnArg::Typed(item) => {
+                    let name = match &*item.pat {
+                        Pat::Ident(p) => &p.ident,
+                        pat => {
+                            // TODO: Support pattern arguments, when decorated with argument with item
+                            // name
+                            //
+                            // Eg.
+                            //
+                            // ```
+                            // fn exec_foo(&self, ctx: Ctx, #[msg(name=metadata)] SomeData { addr, sender }: SomeData);
+                            // ```
+                            //
+                            // should expand to enum variant:
+                            //
+                            // ```
+                            // ExecFoo {
+                            //   metadata: SomeDaa
+                            // }
+                            // ```
+                            let err =
+                                Error::new(pat.span(), "Expected argument name, pattern occurred")
+                                    .into_compile_error();
+                            let name = format!("_invalid_{}", idx);
+                            return quote! {
+                                #name: #err
+                            };
+                        }
+                    };
 
-            let name = Ident::new(&name.to_string().to_case(Case::Camel), name.span());
-            let ty = &item.ty;
+                    let name = Ident::new(&name.to_string().to_case(Case::Snake), name.span());
+                    let ty = &item.ty;
 
-            quote! {
-                #name: #ty
+                    quote! {
+                        #name: #ty
+                    }
+                }
             }
-        }
-    });
+        });
+
+    let fields: Vec<_> = fields.collect();
 
     quote! {
         #name {
