d91d83794d86444ca60ab340bba2d01e888fe6d2
Jakub Bogucki <jakub@confio.gmbh>
2021-12-21 17:15:34 +0100

    Move Threshold and coexisting implementations into packages/utils

--- contracts/cw3-flex-multisig/src/state.rs
+++ contracts/cw3-flex-multisig/src/state.rs
@@ -6,9 +6,7 @@ use cosmwasm_std::{Addr, BlockInfo, CosmosMsg, Decimal, Empty, StdResult, Storag
 use cw3::{Status, Vote};
 use cw4::Cw4Contract;
 use cw_storage_plus::{Item, Map};
-use utils::{Duration, Expiration};
-
-use crate::msg::Threshold;
+use utils::{Duration, Expiration, Threshold};
 
 // we multiply by this when calculating needed_votes in order to round up properly
 // Note: `10u128.pow(9)` fails as "u128::pow` is not yet stable as a const fn"
