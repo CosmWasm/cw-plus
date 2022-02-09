37d71eeadcd273f83e87efd535cced2bda336023
Bartłomiej Kuras <bart.k@confio.gmbh>
2022-02-09 13:37:47 +0100

    Working on instantiation

--- contracts/cw1-whitelist-ng/src/msg.rs
+++ contracts/cw1-whitelist-ng/src/msg.rs
@@ -32,6 +32,60 @@ impl InstantiateMsg {
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case", untagged)]
+enum ExecMsg<T = Empty> {
+    Cw1(Cw1ExecMsg<T>),
+    Whitelist(WhitelistExecMsg),
+}
+
+impl<T> ExecMsg<T> {
+    pub fn dispatch<Contract>(
+        self,
+        deps: DepsMut,
+        env: Env,
+        info: MessageInfo,
+        contract: &Contract,
+    ) -> Result<Response<T>, <Contract as Cw1<T>>::Error>
+    where
+        Contract: Cw1<T> + Whitelist<T, Error = <Contract as Cw1<T>>::Error>,
+    {
+        use ExecMsg::*;
+
+        match self {
+            Cw1(msg) => msg.dispatch(deps, env, info, contract),
+            Whitelist(msg) => msg.dispatch(deps, env, info, contract),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case", untagged)]
+enum QueryMsg<T = Empty> {
+    Cw1(Cw1QueryMsg<T>),
+    Whitelist(WhitelistQueryMsg),
+}
+
+impl<T> QueryMsg<T> {
+    pub fn dispatch<Contract>(
+        self,
+        deps: Deps,
+        env: Env,
+        contract: &Contract,
+    ) -> Result<Binary, <Contract as Cw1<T>>::Error>
+    where
+        Contract: Cw1<T> + Whitelist<T, Error = <Contract as Cw1<T>>::Error>,
+        <Contract as Cw1<T>>::Error: From<StdError>,
+    {
+        use QueryMsg::*;
+
+        match self {
+            Cw1(msg) => msg.dispatch(deps, env, contract),
+            Whitelist(msg) => msg.dispatch(deps, env, contract),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum Cw1ExecMsg<T = Empty> {
     /// Execute requests the contract to re-dispatch all these messages with the
