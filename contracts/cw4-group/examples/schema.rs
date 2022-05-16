d1b336ccf3bf0fdf7da8212fb0e7e4e75c4fde35
Ethan Frey <ethanfrey@users.noreply.github.com>
2022-05-16 22:21:22 +0200

    Combine all files into one json file, each member is a json schema item

--- contracts/cw4-group/examples/schema.rs
+++ contracts/cw4-group/examples/schema.rs
@@ -1,9 +1,36 @@
+use schemars::schema::RootSchema;
 use std::env::current_dir;
-use std::fs::create_dir_all;
+use std::fs::{create_dir_all, write};
 
-use cosmwasm_schema::{export_schema_with_title, remove_schemas, schema_for};
+use cosmwasm_schema::{remove_schemas, schema_for};
 
 pub use cw4_group::msg::{ExecuteMsg, InstantiateMsg, QueryMsg, QueryResponse};
+
+// TODO: move this into export.rs (cosmwasm-schema)
+#[derive(serde::Serialize)]
+struct Api {
+    instantiate: RootSchema,
+    execute: RootSchema,
+    query: RootSchema,
+    response: RootSchema,
+}
+
+impl Api {
+    pub fn set_names(&mut self) {
+        if let Some(metadata) = &mut self.instantiate.schema.metadata {
+            metadata.title = Some("InstantiateMsg".to_string());
+        }
+        if let Some(metadata) = &mut self.execute.schema.metadata {
+            metadata.title = Some("ExecuteMsg".to_string());
+        }
+        if let Some(metadata) = &mut self.query.schema.metadata {
+            metadata.title = Some("QueryMsg".to_string());
+        }
+        if let Some(metadata) = &mut self.response.schema.metadata {
+            metadata.title = Some("QueryResponse".to_string());
+        }
+    }
+}
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -11,8 +38,17 @@ fn main() {
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
 
-    export_schema_with_title(&schema_for!(InstantiateMsg), &out_dir, "InstantiateMsg");
-    export_schema_with_title(&schema_for!(ExecuteMsg), &out_dir, "ExecuteMsg");
-    export_schema_with_title(&schema_for!(QueryMsg), &out_dir, "QueryMsg");
-    export_schema_with_title(&schema_for!(QueryResponse), &out_dir, "QueryResponse");
+    let mut api = Api {
+        instantiate: schema_for!(InstantiateMsg),
+        execute: schema_for!(ExecuteMsg),
+        query: schema_for!(QueryMsg),
+        response: schema_for!(QueryResponse),
+    };
+    api.set_names();
+
+    // TODO: expose write_schema in export.rs (cosmwasm-schema)
+    let path = out_dir.join("api.json".to_string());
+    let json = serde_json::to_string_pretty(&api).unwrap();
+    write(&path, json + "\n").unwrap();
+    println!("Created {}", path.to_str().unwrap());
 }
