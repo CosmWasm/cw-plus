241959f5302d490abdffb1400cd153536b7e5cb7
Tomasz Kurcz <uint@lavabit.com>
2022-10-18 22:25:37 +0200

    Remove cw1155 stuff

--- contracts/cw1155-base/src/msg.rs
+++
@@ -1,9 +0,0 @@
-use cosmwasm_schema::cw_serde;
-
-#[cw_serde]
-pub struct InstantiateMsg {
-    /// The minter is the only one who can create new tokens.
-    /// This is designed for a base token platform that is controlled by an external program or
-    /// contract.
-    pub minter: String,
-}
