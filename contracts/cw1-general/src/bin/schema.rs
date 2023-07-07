4d5ab197af027073e5987e53294b1a64ad2f5ce4
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2023-07-07 19:50:38 -0400

    🎨 housekeeping

--- contracts/cw1-general/src/bin/schema.rs
+++ contracts/cw1-general/src/bin/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use cw1_whitelist::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cw1_general::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
