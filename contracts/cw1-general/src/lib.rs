f7515fb2e7966a61cefe0dc05a6ca4f27b47e811
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2023-07-07 22:38:23 -0400

    🎨 More clean up

--- contracts/cw1-general/src/lib.rs
+++ contracts/cw1-general/src/lib.rs
@@ -1,11 +1,3 @@
-/*!
-This may truly be the simplest implementation of [CW1](https://github.com/CosmWasm/cw-plus/blob/main/packages/cw1/README.md),
-a generalized contract that anybody can call!
-
-For more information on this contract, please check out the
-[README](https://github.com/CosmWasm/cw-plus/blob/main/contracts/cw1-whitelist/README.md).
-*/
-
 pub mod contract;
 pub mod error;
 pub mod msg;
