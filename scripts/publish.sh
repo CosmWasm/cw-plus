409c7a0ce409da196a35ed9b49dbe8fe2adec5d9
Tomasz Kulik <tomek.k@confio.gmbh>
2024-02-15 09:41:35 +0100

    chore: Update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -13,9 +13,8 @@ then
     exit 1
 fi
 
-# these are imported by other packages
-BASE_PACKAGES="cw2"
-ALL_PACKAGES="controllers cw1 cw3 cw4 cw20"
+# These are imported by other packages
+ALL_PACKAGES="cw1 cw3 cw4 cw20"
 
 # This is imported by cw3-fixed-multisig, which is imported by cw3-flex-multisig
 # need to make a separate category to remove race conditions
@@ -25,18 +24,6 @@ BASE_CONTRACTS="cw1-whitelist cw4-group cw3-fixed-multisig "
 ALL_CONTRACTS="cw1-subkeys cw3-flex-multisig cw4-stake cw20-ics20"
 
 SLEEP_TIME=30
-
-for pack in $BASE_PACKAGES; do
-  (
-    cd "packages/$pack"
-    echo "Publishing $pack"
-    cargo publish
-  )
-done
-
-# wait for these to be processed on crates.io
-echo "Waiting for publishing base packages"
-sleep $SLEEP_TIME
 
 for pack in $ALL_PACKAGES; do
   (
